fe403f0c6350d2a0960a4189e4de469461d5a779
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2024-02-18 07:14:18 +0000

    Added static assets

---
+++ config.ts
@@ -0,0 +1,6 @@
+const config = {
+    LOGO_URL: "https://cdn.global.noobsverse.com/Incogni-removebg-preview+(1).png",
+    LOGO_WHITE: "https://cdn.global.noobsverse.com/Incogni.png"
+}
+
+export default config;
